819185e54db73f7fa1625ab6e43b1b280c21cd08
Joel Brigida <joel@drtechreps.com>
2022-01-03 23:54:06 -0500

    initial upload of example code

---
+++ 03-task-mgmt/src/main.cpp
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+
+/**
+ * Joel Brigida
+ * January 3, 2022
+ * This demonstration shows a higher priority task which interrupts a lower priority task
+ * by printing messages to the serial terminal.
+ */
+
+// Use only core 1 for demo purposes
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+// Some string to print
+const char msg[] = "Barkadeer brig Arr booty rum.";
+
+// Task handles
+static TaskHandle_t task_1 = NULL;
+static TaskHandle_t task_2 = NULL;
+
+//*****************************************************************************
+// Tasks
+
+// Task: print to Serial Terminal with lower priority
+void startTask1(void *parameter)
+{
+  // Count number of characters in string
+  int msg_len = strlen(msg);
+
+  // Print string to Terminal
+  for (;;) {
+    Serial.println();
+    for (int i = 0; i < msg_len; i++) {
+      Serial.print(msg[i]);
+    }
+    Serial.println();
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+// Task: print to Serial Terminal with higher priority
+void startTask2(void *parameter) 
+{
+  for (;;) {
+    Serial.print('*');
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+//*****************************************************************************
+// Main (runs as its own task with priority 1 on core 1)
+ 
+void setup() 
+{
+
+  // Configure Serial (go slow so we can watch the preemption)
+  Serial.begin(300);
+
+  // Wait a moment to start (so we don't miss Serial output)
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Task Demo---");
+
+  // Print self priority
+  Serial.print("Setup and loop task running on core ");
+  Serial.print(xPortGetCoreID());
+  Serial.print(" with priority ");
+  Serial.println(uxTaskPriorityGet(NULL));
+
+ // Task to run forever
+  xTaskCreatePinnedToCore(startTask1,
+                            "Task 1",
+                            1024,
+                            NULL,
+                            1,
+                            &task_1,
+                            app_cpu);
+
+  // Task to run once with higher priority
+  xTaskCreatePinnedToCore(startTask2,
+                            "Task 2",
+                            1024,
+                            NULL,
+                            2,
+                            &task_2,
+                            app_cpu);
+}
+
+void loop()
+{
+
+  // Suspend the higher priority task for some intervals
+  for (int i = 0; i < 3; i++) 
+  {
+    vTaskSuspend(task_2);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    vTaskResume(task_2);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+
+  // Delete the lower priority task
+  if (task_1 != NULL) {
+    vTaskDelete(task_1);
+    task_1 = NULL;
+  }
+}
