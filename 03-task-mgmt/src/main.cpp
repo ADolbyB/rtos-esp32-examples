574c2ddbfa39373028721da8acdd01a3d889ae0c
Joel Brigida <joel@drtechreps.com>
2022-01-04 22:45:10 -0500

    updated working example with new comments

--- 03-task-mgmt/src/main.cpp
+++ 03-task-mgmt/src/main.cpp
@@ -7,103 +7,114 @@
  * by printing messages to the serial terminal.
  */
 
-// Use only core 1 for demo purposes
+
+/* Set ESP32 to use only core #1 in this demonstration */
+
 #if CONFIG_FREERTOS_UNICORE
   static const BaseType_t app_cpu = 0;
 #else
   static const BaseType_t app_cpu = 1;
 #endif
 
-// Some string to print
-const char msg[] = "Barkadeer brig Arr booty rum.";
 
-// Task handles
+/* Declare a message for printing to the serial terminal */
+
+const char msg[] = "This message will print to the serial terminal";
+
+int msg_len = strlen(msg);                        // Count number of characters in string
+
+/* Declare 2 tasks in static memory with NULL handles */
+/* Source: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/freertos.html */
+
 static TaskHandle_t task_1 = NULL;
 static TaskHandle_t task_2 = NULL;
+/* 
+ * From <task.h>: [Taskhandle_t] Type by which tasks are referenced.  For example, a call to xTaskCreate()
+ * returns (via a pointer parameter) an TaskHandle_t variable that can then
+ * be used as a parameter to vTaskDelete to delete the task.
+ */
 
-//*****************************************************************************
-// Tasks
 
-// Task: print to Serial Terminal with lower priority
+/* Function definition for Task 1 (will have lower priority) */
+
 void startTask1(void *parameter)
 {
-  // Count number of characters in string
-  int msg_len = strlen(msg);
 
-  // Print string to Terminal
-  for (;;) {
-    Serial.println();
-    for (int i = 0; i < msg_len; i++) {
-      Serial.print(msg[i]);
+  for (;;)
+  {
+    Serial.println();                               // Print a newline '\n' in terminal
+    for (int i = 0; i < msg_len; i++)
+    {
+      
+      Serial.print(msg[i]);                         // Print string to Terminal 1 character at a time
+    
     }
-    Serial.println();
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.println();                               // Print a newline '\n' in terminal
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);          // 1 second delay after printing
   }
 }
 
-// Task: print to Serial Terminal with higher priority
+
+/* Function definition for Task 2 (will have higher priority) */
+
 void startTask2(void *parameter) 
 {
-  for (;;) {
-    Serial.print('*');
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+  for (;;) 
+  {
+    Serial.print('*');                                    // Print a single asterisk to the serial terminal
+    vTaskDelay(100 / portTICK_PERIOD_MS);                 // 0.1 sec display
   }
 }
 
-//*****************************************************************************
-// Main (runs as its own task with priority 1 on core 1)
+
+/* Main code setup: runs on core #1 with priority 1 */
  
 void setup() 
 {
+  Serial.begin(300);                                      // Configure slowest serial speed to view character printing
+  vTaskDelay(1000 / portTICK_PERIOD_MS);                  // Wait 1 sec before output: don't miss terminal output
 
-  // Configure Serial (go slow so we can watch the preemption)
-  Serial.begin(300);
-
-  // Wait a moment to start (so we don't miss Serial output)
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
   Serial.println();
-  Serial.println("---FreeRTOS Task Demo---");
+  Serial.println("=>> ESP32 FreeRTOS Task Demo <<=");
 
-  // Print self priority
-  Serial.print("Setup and loop task running on core ");
+  Serial.print("Setup and loop task running on core ");   // Prints priority of the Setup and Loop functions
   Serial.print(xPortGetCoreID());
   Serial.print(" with priority ");
-  Serial.println(uxTaskPriorityGet(NULL));
+  Serial.println(uxTaskPriorityGet(NULL));                // NULL argument designates the calling function
 
- // Task to run forever
-  xTaskCreatePinnedToCore(startTask1,
-                            "Task 1",
-                            1024,
-                            NULL,
-                            1,
-                            &task_1,
-                            app_cpu);
+  xTaskCreatePinnedToCore(startTask1,                     // Function Name: startTask1 runs forever
+                          "Task 1",                       // Name of task
+                          1024,                           // Stack size of task (minimum 768 for empty task)
+                          NULL,                           // Task Parameter
+                          1,                              // Task Priority
+                          &task_1,                        // Task handle for tracking task
+                          app_cpu);                       // CPU Core which runs task (app_cpu = 1)
 
-  // Task to run once with higher priority
-  xTaskCreatePinnedToCore(startTask2,
-                            "Task 2",
-                            1024,
-                            NULL,
-                            2,
-                            &task_2,
-                            app_cpu);
+  xTaskCreatePinnedToCore(startTask2,                     // Function Name: startTask1 runs once with higher priority
+                          "Task 2",                       // Name of task
+                          1024,                           // Stack size of task (minimum 768 for empty task)
+                          NULL,                           // Task Parameter
+                          2,                              // Task Priority
+                          &task_2,                        // Task handle for tracking task
+                          app_cpu);                       // CPU Core which runs task 
 }
 
 void loop()
 {
 
-  // Suspend the higher priority task for some intervals
   for (int i = 0; i < 3; i++) 
   {
-    vTaskSuspend(task_2);
+    vTaskSuspend(task_2);                                 // Suspend task_2 for 2 seconds
     vTaskDelay(2000 / portTICK_PERIOD_MS);
-    vTaskResume(task_2);
+    vTaskResume(task_2);                                  // Resume task_2 for 2 seconds (task_2 prints '*' every 0.1 sec)
     vTaskDelay(2000 / portTICK_PERIOD_MS);
   }
 
-  // Delete the lower priority task
-  if (task_1 != NULL) {
-    vTaskDelete(task_1);
+  if (task_1 != NULL) 
+  {
+    vTaskDelete(task_1);                                  // Delete task_1 (task_1 prints message to the serial terminal)
     task_1 = NULL;
   }
+
 }
